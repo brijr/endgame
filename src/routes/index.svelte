ad2360817e83a0e3e35035fff05d356c5d00fc87
Birdger Tower <57158102+brijr@users.noreply.github.com>
2022-07-06 19:37:40 -0600

    update padding

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
-<h1 class="text-2xl">Welcome to Endgame Market</h1>
+<h1 class="text-2xl py-4">Welcome to Endgame Market</h1>
 <p>A place to buy and sell awesome tech.</p>
 
 <p>Created by <a href="https://cameronyoungblood.com">Cameron Youngblood</a> and <a href="https://bridger.info">Bridger Tower</a>.</p>
