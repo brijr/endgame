f72beecec462130147d4054fbc793699dbe4eecb
Bridger Tower <bridgertower@gmail.com>
2022-07-06 19:06:36 -0600

    Create Endgame

---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<h1>Welcome to Endgame Market</h1>
+<p>A place to buy and sell awesome tech.</p>
+
+<p>Created by <a href="https://cameronyoungblood.com">Cameron Youngblood</a> and <a href="https://bridger.info">Bridger Tower</a>.</p>
